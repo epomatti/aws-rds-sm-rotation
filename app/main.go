aa866502c4e1eda4cc5c79e0f80c8caa9263f0b6
Evandro Pomatti <evandro.pomatti@gmail.com>
2023-08-30 17:43:37 -0300

    go secretsmanager sdk

--- app/main.go
+++ app/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"context"
+	"main/utils"
+
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+)
+
 func main() {
 
+	cfg, err := config.LoadDefaultConfig(context.TODO())
+	utils.Check(err)
+
+	client := secretsmanager.NewFromConfig(cfg)
+
+	var secretId string = "rds!db-10db40ec-cebd-4ade-9865-a4b99718a0a1"
+
+	input := &secretsmanager.GetSecretValueInput{
+		SecretId: &secretId,
+	}
+
+	v, err := client.GetSecretValue(context.TODO(), input)
+	utils.Check(err)
+
+	println(*v.SecretString)
 }
